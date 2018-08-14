c7ede582a2ed92de0db358a2499125ca6f2e1c98
HypnoMama <sarahhermsen@gmail.com>
2018-08-14 00:51:32 +0000

    removed dead code and whitespace

--- math_question.rb
+++ math_question.rb
@@ -22,26 +22,6 @@ class MathQuestion
 
     def create_question
         question = "#{first_num} #{operator} #{second_num}"
-        # puts "What is the answer to: #{question}"
     end
 
-
-
-    # def check_answer(answer)
-    #     answer == self.correct_answer
-    # end
-
-
 end
-
-# question needs to be how it is.  saved as a variable in game,
-# then check the answer by passing it to check_answer or correct answer to eval it
-
-# a_question = MathQuestion.new()
-# puts a_question.first_num
-# puts a_question.second_num
-# question =  a_question.create_question
-# puts question
-# answer = a_question.correct_answer(question)
-# puts answer
-# puts a_question.check_answer(answer)
